6eaee79b6d0a8e8c8a7ead024ab315c668a9fb3a
EnglishCheese <145911165+EnglishCheese@users.noreply.github.com>
2023-10-22 17:13:17 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from PyQt5. QtWidgets import QApplication
-
+from random import choice, shuffle
+from time import sleep
 app = QApplication([])
 
 from m2 import *
 from m3 import *
 
+class Question:
+    def __init__(self,question, answer, wrong_answer1, wrong_answer2, wrong_answer3):
+        self.question = question
+        self.answer = answer
+        self.wrong_answer1 = wrong_answer1
+        self.wrong_answer2 = wrong_answer2
+        self.wrong_answer3 = wrong_answer3
+q1 = Question('Де знаходиться вавилон?', 'Ірак', 'Іран', 'Індія','Франція' )
+q2 = Question('Коли почалася перша світова війна', 'В 1914 р.','В 1913 р.', 'В 1924 р.', 'В 1916')
+q3 = Question('Що можна утримувати не торкаючись його руками','Дихання', 'Мовчання', 'Світло', '')
+q4 = Question('З якого посуду нічого не можна їсти','З порожнього','З перевернутого', 'З гарячого', "З кам'яного")
+q5 = Question('','','','','')
+q6 = Question('','','','','')
+q7 = Question('','','','','')
+q8 = Question('','','','','')
+q9 = Question('','','','','')
+q10 = Question('','','','','')
 def menu_generation():
     menu_win.show()
     window.hide()
